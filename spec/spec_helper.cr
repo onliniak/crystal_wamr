a01d143c924ecc988fa9fbb1ce57e8bd5d893eb1
Rafael Pszenny <onliniak@gmail.com>
2024-07-22 16:06:14 +0200

    Update spec_helper.cr

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,19 @@
 require "spec"
 require "../src/crystal_wamr"
+require "benchmark"
+
+def collatz(c, only_result : Bool)
+  # start : Float64 = c.to_f64
+  argv = Array(Int32 | Float64).new
+  # argv << start
+  while c != 1
+    if c % 2 > 0
+      c = 3 * c + 1
+    else
+      c /= 2
+    end
+    argv << c
+  end
+  p argv unless only_result
+  p "#N : #{argv.size}"
+end
