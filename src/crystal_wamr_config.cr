5e17b74cb795be3d251570f11690e610f0546e2d
Rafael Pszenny <onliniak@gmail.com>
2024-07-28 17:20:56 +0200

    breaking: JSON config

---
+++ src/crystal_wamr_config.cr
@@ -0,0 +1,38 @@
+require "json"
+
+module CrystalWamr
+  class WamrConfig
+    include JSON::Serializable
+
+    property file : String
+    property func : Array(Func)
+  end
+
+  class Func
+    include JSON::Serializable
+
+    property name : String
+    property input : Array(Input)
+  end
+
+  class Input
+    include JSON::Serializable
+
+    property argv : Argv
+  end
+
+  class Argv
+    include JSON::Serializable
+
+    property int : Int32?
+    property var : String?
+    property sys : Sys?
+  end
+
+  class Sys
+    include JSON::Serializable
+
+    property name : String
+    property argv : Array(Int32?)
+  end
+end
